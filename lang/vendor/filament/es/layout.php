e102e11295cdac4b87ddce36aba22a4ef2f8dab7
Francisco Montenegro <franciscomontenegro22@gmail.com>
2022-07-02 10:44:27 -0300

    added dark mode, lang ar, and change some pameters to dashboard

---
+++ lang/vendor/filament/es/layout.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'direction' => 'ltr',
+
+    'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'Ver en modo oscuro',
+        ],
+
+        'light_mode' => [
+            'label' => 'Ver en modo claro',
+        ],
+
+        'logout' => [
+            'label' => 'Salir',
+        ],
+    ],
+
+];
